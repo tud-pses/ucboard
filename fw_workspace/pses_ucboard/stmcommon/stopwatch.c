1ba365126da205b3ab17d4d5dc4c39b6cdb82015
elenz <elenz@iat.tu-darmstadt.de>
2016-09-15 21:58:26 +0200

    stopwatch hinzugefügt (TIM15->TIM20)

---
+++ fw_workspace/pses_ucboard/stmcommon/stopwatch.c
@@ -0,0 +1,95 @@
+
+#include "stm32f3xx.h"
+//#include "stm32f3xx_hal_conf.h"
+
+#include "stopwatch.h"
+#include "stm32f3xx_hal_tim.h"
+//#include <stdint.h>
+
+extern void Error_Handler(void);
+
+static TIM_HandleTypeDef htim15;
+static TIM_HandleTypeDef htim20;
+
+void stopwatch_init( void )
+{
+	__HAL_RCC_TIM15_CLK_ENABLE();
+	__HAL_RCC_TIM20_CLK_ENABLE();
+
+	//TIM_MasterConfigTypeDef sMasterConfig;
+	//TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig;
+	//TIM_OC_InitTypeDef sConfigOC;
+
+	// Timer läuft mit 1MHz (72 MHz Prozessor, 72er Prescaler)
+	htim15.Instance = TIM15;
+	htim15.Init.Prescaler = 71;
+	htim15.Init.CounterMode = TIM_COUNTERMODE_UP;
+	htim15.Init.Period = 0xFFFF;
+	htim15.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	htim15.Init.RepetitionCounter = 0;
+	if (HAL_TIM_Base_Init(&htim15) != HAL_OK)
+	{
+		Error_Handler();
+	}
+
+	htim20.Instance = TIM20;
+	htim20.Init.Prescaler = 0;
+	htim20.Init.CounterMode = TIM_COUNTERMODE_UP;
+	htim20.Init.Period = 0xFFFF;
+	htim20.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	htim20.Init.RepetitionCounter = 0;
+	if (HAL_TIM_Base_Init(&htim20) != HAL_OK)
+	{
+		Error_Handler();
+	}
+
+	//TIM15->DIER = 0;
+	__HAL_TIM_CLEAR_FLAG( &htim15, TIM_FLAG_UPDATE );
+	__HAL_TIM_CLEAR_FLAG( &htim20, TIM_FLAG_UPDATE );
+
+
+
+	/*
+
+	TIM_ITConfig( TIM4, TIM_IT_Update, ENABLE );
+	*/
+
+	// Timer 20 ist Slave von Timer 15
+	// CR2[6:4] = 010 -> Update-Mode (Timer 15 ist Prescaler für Timer 16)
+	// Bit 4-6 löschen
+	TIM15->CR2 &= ~(uint16_t)(0x7 << 4);
+	// Folge 010 in Bits 4-6 schreiben
+	TIM15->CR2 |= (uint16_t)(0x2 << 4);
+
+
+	// Timer 20 ist Slave
+	// SMCR[6:4] = 011 -> Master ist TIM20
+	// SMCR[2:0] = 111 -> External Clock Mode
+
+	// Bit 4-6 löschen
+	TIM20->SMCR &= ~(uint16_t)(0x7 << 4);
+	// Folge 010 in Bits 4-6 schreiben
+	TIM20->SMCR |= (uint16_t)(0x3 << 4);
+
+	// Folge 111 in Bits 0-2 schreiben
+	TIM20->SMCR |= (uint16_t)0x7;
+
+	TIM15->CNT = 0;
+	TIM20->CNT = 0;
+
+	__HAL_TIM_ENABLE_IT(&htim15, TIM_IT_UPDATE);
+
+	HAL_TIM_Base_Start(&htim20);
+	HAL_TIM_Base_Start(&htim15);
+
+	return;
+}
+
+
+void TIM15_IRQHandler( void )
+{
+	__HAL_TIM_CLEAR_FLAG( &htim15, TIM_FLAG_UPDATE );
+
+	return;
+}
+
---
+++ fw_workspace/pses_ucboard/stmcommon/stopwatch.h
@@ -0,0 +1,90 @@
+
+
+// stopwatch.h
+
+#ifndef _STOPWATCH_H_
+#define _STOPWATCH_H_
+//#include 
+
+#include "stm32f3xx_hal_tim.h"
+#include <stdint.h>
+
+void stopwatch_init( void );
+static inline uint32_t stopwatch_getTic( void );
+static inline uint32_t stopwatch_getDeltaTime_us( uint32_t uTic );
+
+// Die wait-Funktionen der Stopwatch garantieren die Mindest(!)wartedauer,
+// bis sie beendet werden. Die Wartezeit kann aber über der angebenen
+// Wunschdauer liegen, wenn Interrupts höherer Priorität auftreten.
+static inline void stopwatch_wait_us(uint32_t uDuration);
+static inline void stopwatch_wait_ms(uint32_t uDuration);
+
+static inline void stopwatch_wait_us_tic(uint32_t uDuration, uint32_t uTic);
+
+// Annahme: getTic wird nie für mehr als 64ms durch andere Interrupts blockiert,
+// so dass maximal ein Umbruch des TIM2 auftritt.
+// (Man könnte diese Funktion auch atomar machen, um diese Bedingung sicher
+// einzuhalten.)
+static inline uint32_t stopwatch_getTic( void )
+{
+	uint32_t uLoHalfWordBefore, uHiHalfWord, uLoHalfWordAfter;
+	volatile uint32_t* pTim15Cnt = &TIM15->CNT;
+	volatile uint32_t* pTim16Cnt = &TIM16->CNT;
+
+	uLoHalfWordBefore = *pTim15Cnt;
+	uHiHalfWord = *pTim16Cnt;
+	uLoHalfWordAfter = *pTim15Cnt;
+
+	if (uLoHalfWordBefore <= uLoHalfWordAfter)
+	{
+		return ((uint32_t)uHiHalfWord << 16) | uLoHalfWordAfter;
+	}
+	else
+	{
+		uHiHalfWord = *pTim16Cnt;
+		return ((uint32_t)(uHiHalfWord) << 16) | uLoHalfWordAfter;
+	}
+}
+
+static inline uint32_t stopwatch_getDeltaTime_us( uint32_t uTic )
+{
+	uint32_t uToc;
+
+	uToc = stopwatch_getTic();
+
+	if (uToc < uTic)
+	{
+		return (0xFFFFFFFF - uTic) + uToc;
+	}
+	else
+	{
+		return (uToc - uTic);
+	}
+}
+
+
+static inline void stopwatch_wait_us(uint32_t uDuration)
+{
+	uint32_t uTic = stopwatch_getTic();
+
+	while(stopwatch_getDeltaTime_us(uTic) < uDuration) {}
+
+	return;
+}
+
+static inline void stopwatch_wait_ms(uint32_t uDuration)
+{
+	stopwatch_wait_us(uDuration * 1000);
+
+	return;
+}
+
+static inline void stopwatch_wait_us_tic(uint32_t uDuration, uint32_t uTic)
+{
+	while(stopwatch_getDeltaTime_us(uTic) < uDuration) {}
+
+	return;
+}
+
+
+#endif //_STOPWATCH_H_
