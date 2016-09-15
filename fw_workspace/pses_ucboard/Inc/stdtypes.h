5f753d7070829d0dacab8a566220024e606f6d9a
elenz <elenz@iat.tu-darmstadt.de>
2016-09-15 23:07:42 +0200

    stdtypes hinzugefügt

---
+++ fw_workspace/pses_ucboard/Inc/stdtypes.h
@@ -0,0 +1,31 @@
+/*
+ * stdtypes.h
+ *
+ *  Created on: 02.01.2010
+ *      Author: eric
+ */
+
+#ifndef STDTYPES_H_
+#define STDTYPES_H_
+
+#include <stdint.h>
+
+#include <stdbool.h>
+
+#ifndef FALSE
+#define FALSE false
+#endif
+#ifndef TRUE
+#define TRUE true
+#endif
+
+
+//#define NULL 0
+#include <stdio.h>
+
+
+#define UNUSED(x) UNUSED_ ## x __attribute__((unused))
+
+
+
+#endif /* STDTYPES_H_ */
