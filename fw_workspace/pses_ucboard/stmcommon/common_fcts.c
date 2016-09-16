166560fea928622067292dc0dcef91318059a5d1
elenz <elenz@iat.tu-darmstadt.de>
2016-09-16 15:54:28 +0200

    common_fcts hinzugefügt

---
+++ fw_workspace/pses_ucboard/stmcommon/common_fcts.c
@@ -0,0 +1,97 @@
+/*
+ * common_fcts.c
+ *
+ *  Created on: 02.12.2009
+ *      Author: elenz
+ */
+
+#include "stm32f3xx.h"
+
+#include "common_fcts.h"
+
+
+
+
+/**
+ * C++ version 0.4 char* style "itoa":
+ * Written by Lukás Chmela
+ * Released under GPLv3.
+ *
+ * Specifier für zweites Argument von char* auf
+ * char * const geändert. (Um Warnungen zu vermeiden,
+ * wenn Argument ein Array ist.)
+ */
+char* itoa(int value, char * const result, int base) {
+	// check that the base if valid
+	if (base < 2 || base > 36)
+	{
+		*result = '\0';
+		return result;
+	}
+
+	char* ptr = result, *ptr1 = result, tmp_char;
+	int tmp_value;
+
+	do {
+		tmp_value = value;
+		value /= base;
+		*ptr++ = "ZYXWVUTSRQPONMLKJIHGFEDCBA9876543210123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ" [35 + (tmp_value - value * base)];
+	} while ( value );
+
+	// Apply negative sign
+	if (tmp_value < 0)
+	{
+		*ptr++ = '-';
+	}
+
+	*ptr-- = '\0';
+
+	while(ptr1 < ptr)
+	{
+		tmp_char = *ptr;
+		*ptr-- = *ptr1;
+		*ptr1++ = tmp_char;
+	}
+
+	return result;
+}
+
+/**
+ * (Minimale) Adaption der Funktion itoa auf unsigned-Werte
+ */
+char* utoa(uint32_t value, char * const result, int base) {
+	// check that the base if valid
+	if (base < 2 || base > 36)
+	{
+		*result = '\0';
+		return result;
+	}
+
+	char* ptr = result, *ptr1 = result, tmp_char;
+	uint32_t tmp_value;
+
+	if (base == 8)
+	{
+		*ptr++ = 'o';
+	}
+	else if (base == 16)
+	{
+		*ptr++ = 'h';
+	}
+
+	do {
+		tmp_value = value;
+		value /= base;
+		*ptr++ = "ZYXWVUTSRQPONMLKJIHGFEDCBA9876543210123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ" [35 + (tmp_value - value * base)];
+	} while ( value );
+
+	*ptr-- = '\0';
+	while(ptr1 < ptr)
+	{
+		tmp_char = *ptr;
+		*ptr-- = *ptr1;
+		*ptr1++ = tmp_char;
+	}
+
+	return result;
+}
---
+++ fw_workspace/pses_ucboard/stmcommon/common_fcts.h
@@ -0,0 +1,123 @@
+/*
+ * common_fcts.h
+ *
+ *  Created on: 02.12.2009
+ *      Author: elenz
+ */
+
+#ifndef COMMON_FCTS_H_
+#define COMMON_FCTS_H_
+
+
+#include "stdtypes.h"
+#include "stopwatch.h"
+
+#define wait_ms(t) Stopwatch_wait_ms(t)
+#define wait_us(t) Stopwatch_wait_us(t)
+
+extern char* itoa(int value, char* const result, int base);
+extern char* utoa(uint32_t value, char* const result, int base);
+
+inline uint32_t copystring(char* szTarget, char* szSource, uint32_t uMaxLen);
+
+
+inline void memcpyX(uint8_t* pTarget, uint8_t* pSource, uint16_t uLen);
+inline void memcpy8(uint8_t* pTarget, uint8_t* pSource);
+inline void memcpy16(uint8_t* pTarget, uint8_t* pSource);
+inline void memcpy32(uint8_t* pTarget, uint8_t* pSource);
+inline void memcpy64(uint8_t* pTarget, uint8_t* pSource);
+
+inline bool memcmpX(uint8_t* pA, uint8_t* pB, uint16_t uLen);
+
+
+#define MEMCPYX(t, s, len) memcpyX((uint8_t*)t, (uint16_t*)s, len)
+#define MEMCPY8(t, s) memcpy8((uint8_t*)t, (uint8_t*)s)
+#define MEMCPY16(t, s) memcpy16((uint8_t*)t, (uint8_t*)s)
+#define MEMCPY32(t, s) memcpy32((uint8_t*)t, (uint8_t*)s)
+#define MEMCPY64(t, s) memcpy64((uint8_t*)t, (uint8_t*)s)
+
+
+
+#define SATURATION_L(value, llimit) (value<llimit) ? llimit : value
+#define SATURATION_U(value, ulimit) (value>ulimit) ? ulimit : value
+#define SATURATION_LU(value, llimit, ulimit) (value<llimit) ? llimit : ( (value>ulimit) ? ulimit : value)
+
+
+
+inline uint32_t copystring(char* szTarget, char* szSource, uint32_t uMaxLen)
+{
+	uint32_t uLen = 1;
+
+	*szTarget = *szSource;
+	uMaxLen--;
+
+	while ( (*szSource != '\0') && (uMaxLen-- != 0) )
+	{
+		*++szTarget = *++szSource;
+		uLen++;
+	}
+
+	if (*szSource != '\0')
+		*szTarget = '\0';
+
+	return uLen;
+}
+
+
+inline void memcpyX(uint8_t* pTarget, uint8_t* pSource, uint16_t uLen)
+{
+	while(uLen--)
+		*pTarget++ = *pSource++;
+
+	return;
+}
+
+inline void memcpy8(uint8_t* pTarget, uint8_t* pSource)
+{
+	*pTarget = *pSource;
+	return;
+}
+
+inline void memcpy16(uint8_t* pTarget, uint8_t* pSource)
+{
+	*pTarget++ = *pSource++;
+	*pTarget = *pSource;
+	return;
+}
+
+inline void memcpy32(uint8_t* pTarget, uint8_t* pSource)
+{
+	*pTarget++ = *pSource++;
+	*pTarget++ = *pSource++;
+	*pTarget++ = *pSource++;
+	*pTarget = *pSource;
+	return;
+}
+
+inline void memcpy64(uint8_t* pTarget, uint8_t* pSource)
+{
+	*pTarget++ = *pSource++;
+	*pTarget++ = *pSource++;
+	*pTarget++ = *pSource++;
+	*pTarget++ = *pSource++;
+	*pTarget++ = *pSource++;
+	*pTarget++ = *pSource++;
+	*pTarget++ = *pSource++;
+	*pTarget = *pSource;
+	return;
+}
+
+inline bool memcmpX(uint8_t* pA, uint8_t* pB, uint16_t uLen)
+{
+	while (uLen--)
+	{
+		if (*pA++ != *pB++)
+			return FALSE;
+	}
+
+	return TRUE;
+}
+
+
+
+#endif /* COMMON_FCTS_H_ */
