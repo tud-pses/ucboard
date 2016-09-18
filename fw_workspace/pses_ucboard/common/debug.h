3c527a715457d191ca5944de7c96c8f08d5f5860
elenz <elenz@iat.tu-darmstadt.de>
2016-09-18 12:58:46 +0200

    debug.h hinzugefügt (noch "leere" Makros)

---
+++ fw_workspace/pses_ucboard/common/debug.h
@@ -0,0 +1,18 @@
+/*
+ * debug.h
+ *
+ *  Created on: 18.09.2016
+ *      Author: elenz
+ */
+
+#ifndef DEBUG_H_
+#define DEBUG_H_
+
+
+
+#define DEBUG_OUT_HEX(a, b)
+#define DEBUG_OUT(a)
+#define DEBUG_OUT_UINT(a, b)
+
+
+#endif /* DEBUG_H_ */
