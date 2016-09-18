d40b3d3c8bb39b9b094b6b0a4b73b54de896d882
elenz <elenz@iat.tu-darmstadt.de>
2016-09-18 14:27:43 +0200

    Datei für Session-ID hinzugefügt

---
+++ fw_workspace/pses_ucboard/devices/sid.c
@@ -0,0 +1,21 @@
+/*
+ * sid.c
+ *
+ *  Created on: 18.09.2016
+ *      Author: elenz
+ */
+
+
+#include "stdtypes.h"
+
+static uint32_t f_sid = 0;
+
+
+
+bool cmd_sessionid(uint8_t* acData, uint8_t nLen,
+								uint8_t* acRespData, uint8_t* pnRespLen)
+{
+
+	return true;
+}
+
