198a2d4168d4e3f810fc5922264edd010388ba00
elenz <elenz@iat.tu-darmstadt.de>
2016-09-23 17:52:26 +0200

    Test für Taster und LEDs in systick

--- fw_workspace/pses_ucboard/Src/systick.c
+++ fw_workspace/pses_ucboard/Src/systick.c
@@ -1,8 +1,37 @@
 
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "ucboard_hwfcts.h"
+
+static uint32_t f_uTic = 0;
+
+
+uint32_t systick_getTics()
+{
+	return f_uTic;
+}
 
 
 void HAL_SYSTICK_Callback(void)
 {
+	++f_uTic;
+
+	if (BUTTON_A_ISPRESSED() || BUTTON_B_ISPRESSED() || BUTTON_C_ISPRESSED())
+	{
+		LED_SYS_ON();
+		LED_DRVBAT_ON();
+		LED_A_ON();
+		LED_B_ON();
+	}
+	else
+	{
+		LED_SYS_OFF();
+		LED_DRVBAT_OFF();
+		LED_A_OFF();
+		LED_B_OFF();
+	}
+
 	return;
 }
 
