5a17ff690129ab0f4b91c3a689b6037369270f6d
elenz <elenz@iat.tu-darmstadt.de>
2016-09-23 17:52:09 +0200

    Taster und LEDs definiert

---
+++ fw_workspace/pses_ucboard/devices/ucboard_hwdefs.h
@@ -0,0 +1,35 @@
+/*
+ * ucboard_hwdefs.h
+ *
+ *  Created on: 23.09.2016
+ *      Author: elenz
+ */
+
+#ifndef UCBOARD_HWDEFS_H_
+#define UCBOARD_HWDEFS_H_
+
+
+#include "stm32f3xx.h"
+
+
+#define BUTTON_A_PORT 			GPIOC
+#define BUTTON_A_PIN			GPIO_PIN_13
+#define BUTTON_B_PORT 			GPIOC
+#define BUTTON_B_PIN			GPIO_PIN_14
+#define BUTTON_C_PORT 			GPIOC
+#define BUTTON_C_PIN			GPIO_PIN_15
+
+
+#define LED_SYS_PORT		GPIOA
+#define LED_SYS_PIN			GPIO_PIN_11
+#define LED_DRVBAT_PORT		GPIOC
+#define LED_DRVBAT_PIN		GPIO_PIN_8
+#define LED_A_PORT			GPIOC
+#define LED_A_PIN			GPIO_PIN_4
+#define LED_B_PORT			GPIOC
+#define LED_B_PIN			GPIO_PIN_5
+
+
+
+
+#endif /* UCBOARD_HWDEFS_H_ */
---
+++ fw_workspace/pses_ucboard/devices/ucboard_hwfcts.h
@@ -0,0 +1,37 @@
+/*
+ * ucboard_hwfcts.h
+ *
+ *  Created on: 23.09.2016
+ *      Author: elenz
+ */
+
+#ifndef UCBOARD_HWFCTS_H_
+#define UCBOARD_HWFCTS_H_
+
+#include "ucboard_hwdefs.h"
+
+
+
+#define BUTTON_A_ISPRESSED()			(!(BUTTON_A_PORT->IDR & BUTTON_A_PIN))
+#define BUTTON_B_ISPRESSED()			(!(BUTTON_B_PORT->IDR & BUTTON_B_PIN))
+#define BUTTON_C_ISPRESSED()			(!(BUTTON_C_PORT->IDR & BUTTON_C_PIN))
+
+
+#define LED_SYS_ON()		(LED_SYS_PORT->BSRR = LED_SYS_PIN)
+#define LED_SYS_OFF()		(LED_SYS_PORT->BRR = LED_SYS_PIN)
+#define LED_SYS_TOGGLE()	if (LED_SYS_PORT->ODR & LED_SYS_PIN) LED_SYS_OFF(); else LED_SYS_ON()
+
+#define LED_DRVBAT_ON()		(LED_DRVBAT_PORT->BSRR = LED_DRVBAT_PIN)
+#define LED_DRVBAT_OFF()	(LED_DRVBAT_PORT->BRR = LED_DRVBAT_PIN)
+#define LED_DRVBAT_TOGGLE()	if (LED_DRVBAT_PORT->ODR & LED_DRVBAT_PIN) LED_DRVBAT_OFF(); else LED_DRVBAT_ON()
+
+#define LED_A_ON()			(LED_A_PORT->BSRR = LED_A_PIN)
+#define LED_A_OFF()			(LED_A_PORT->BRR = LED_A_PIN)
+#define LED_A_TOGGLE()		if (LED_A_PORT->ODR & LED_A_PIN) LED_A_OFF(); else LED_A_ON()
+
+#define LED_B_ON()			(LED_B_PORT->BSRR = LED_B_PIN)
+#define LED_B_OFF()			(LED_B_PORT->BRR = LED_B_PIN)
+#define LED_B_TOGGLE()		if (LED_B_PORT->ODR & LED_B_PIN) LED_B_OFF(); else LED_B_ON()
+
+
+#endif /* UCBOARD_HWFCTS_H_ */
