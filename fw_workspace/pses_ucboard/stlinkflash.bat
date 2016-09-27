b25f43ef1d935ce3ff4c9ea555193ea1cc686165
elenz <elenz@iat.tu-darmstadt.de>
2016-09-27 10:35:35 +0200

    Batch-Datei zum Flashen hinzugefügt

---
+++ fw_workspace/pses_ucboard/stlinkflash.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\st-link utility\st-link_cli" -P "%~1" 0x08000000 -V "after_programming"
+
+"C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\st-link utility\st-link_cli" -Rst
