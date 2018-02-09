00e4e188d7e7381ab50ddb2c1b126a097f460f78
Eric Lenz <elenz@iat.tu-darmstadt.de>
2018-02-09 16:17:00 +0100

    - UART3 deaktiviert

--- fw_workspace/pses_ucboard/Src/main.c
+++ fw_workspace/pses_ucboard/Src/main.c
@@ -159,7 +159,7 @@ int main(void)
   //MX_TIM8_Init();
   //MX_UART4_Init();
   MX_USART2_UART_Init();
-  MX_USART3_UART_Init();
+  //MX_USART3_UART_Init();
   MX_TIM6_Init();
 
   /* USER CODE BEGIN 2 */
